401965a57037fc9f18ff172b400d92b938e6b0f3
pylong <jspeter12@gmail.com>
2019-11-15 16:37:58 +0100

    06 blog app mid

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Blog1(models.Model):
+    title_b = models.CharField(max_length=255)
+    pub_date_b = models.DateField()
+    body_b = models.TextField()
+    image_b = models.ImageField(upload_to='images/')
+
+
+#add the Blog app to the settings
+
+#create migration
+
+#migrate
+
+#Add to the admin
