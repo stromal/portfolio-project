61e020ff882e06c9fce087a4be93a07893515409
pylong <jspeter12@gmail.com>
2019-12-09 11:03:20 +0100

    15 req2

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
+"""
 import os
 import sys
 
-"""
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
     try:
